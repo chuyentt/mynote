863346cbe3d1fd67582811939c4d84f87525b11b
Chuyen Trung Tran <chuyentt@gmail.com>
2020-11-28 08:33:29 +0700

    Cập nhật data model

--- lib/ui/views/note/note_model.dart
+++ lib/ui/views/note/note_model.dart
@@ -1,6 +1,28 @@
+import 'package:flutter/material.dart';
+
 class Note {
+  /// id tự sinh ra ngẫu nhiên
+  final String id = UniqueKey()
+      .hashCode
+      .toUnsigned(20)
+      .toRadixString(16)
+      .padLeft(5, '0');
+
   final String title;
   final String desc;
+  bool isDeleted = false;
 
   Note(this.title, this.desc);
+
+  /// Tên của bảng CSDL, nó nên được gán sẵn và có thể lấy ra từ data
+  /// model mà không cần khởi tạo nên nó là static để dễ sử dung.
+  static String get tableName => 'Notes';
+
+  /// Chuỗi lệnh SQL để tạo bảng CSDL, nó nên được thiết lập để tạo bảng
+  /// trong CSDL mà không cần khởi tạo nên nó là static để dễ sử dụng.
+  static String get createTable =>
+      'CREATE TABLE $tableName(`id` TEXT PRIMARY KEY,'
+      ' `title` TEXT,'
+      ' `desc` TEXT,'
+      ' `isDeleted` INTEGER DEFAULT 0)';
 }
