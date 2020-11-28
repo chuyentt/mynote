b193bf575e47fc9d80ef0eed47fa0ce8f3e486b2
Chuyen Trung Tran <chuyentt@gmail.com>
2020-11-28 12:19:39 +0700

    Cập nhật note viewmodel

--- lib/ui/views/note/note_viewmodel.dart
+++ lib/ui/views/note/note_viewmodel.dart
@@ -1,5 +1,60 @@
+import 'package:mynote/ui/views/note/note_repository.dart';
 import 'package:stacked/stacked.dart';
+
+import 'note_model.dart';
+
+/// Trạng thái của view
+enum NoteViewState { listView, itemView, insertView, updateView }
 
 class NoteViewModel extends BaseViewModel {
   final title = 'Note View Model';
+
+  /// Danh sách các bản ghi được load bất đồng bộ bên trong view model,
+  /// khi load thành công thì thông báo đến view để cập nhật trạng thái
+  var _items = <Note>[];
+
+  /// ### Danh sách các bản ghi dùng để hiển thị trên ListView
+  /// Vì quá trình load items là bất đồng bộ nên phải tạo một getter
+  /// `get items => _items` để tránh xung đột
+  List<Note> get items => _items;
+
+  /// Trạng thái mặc định của view là listView, nó có thể thay đổi
+  /// bên trong view model
+  var _state = NoteViewState.listView;
+
+  /// Khi thay đổi trạng thái thì sẽ báo cho view biết để cập nhật
+  /// nên cần tạo một setter để vừa nhận giá trị vừa thông báo đến view
+  set state(value) {
+    // Cập nhật giá trị cho biến _state
+    _state = value;
+
+    // Thông báo cho view biết để cập nhật trạng thái của widget
+    notifyListeners();
+  }
+
+  /// Cần có một getter để lấy ra trạng thái view cục bộ cho view
+  NoteViewState get state => _state;
+
+  ///
+  var repo = NoteRepository();
+
+  Future init() async {
+    return reloadItems();
+  }
+
+  Future reloadItems() async {
+    return repo.items().then((value) {
+      _items = value;
+      notifyListeners();
+    });
+  }
+
+  void addItem() {
+    var title = DateTime.now().millisecondsSinceEpoch.toString();
+    var desc = DateTime.now().toLocal().toString();
+    var item = Note(title, desc);
+    repo.insert(item).then((value) {
+      reloadItems();
+    });
+  }
 }
