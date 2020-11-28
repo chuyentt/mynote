610d4e00ef5a8f2d239a585396b05500ea7bb6d3
Chuyen Trung Tran <chuyentt@gmail.com>
2020-11-28 09:04:09 +0700

    Bổ sung abstraction repository cho các data model để CRUD

---
+++ lib/repostory/repository.dart
@@ -0,0 +1,15 @@
+import 'local_repository.dart';
+
+/// Lớp trừu tượng để cho các model thừa kế và dùng trong việc
+/// thêm / sửa / xóa và lấy dữ liệu (Create, Read, Update, Delete - CRUD)
+abstract class Repository<T> {
+  LocalRepository localRepository;
+
+  Future<dynamic> insert(T item);
+
+  Future<dynamic> update(T item);
+
+  Future<dynamic> delete(T item);
+
+  Future<List<T>> items();
+}
